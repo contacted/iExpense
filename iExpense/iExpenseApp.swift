6788e2b84f9f4c56031c55aba8cf1076ef0b887d
Eugene <contact.duk@gmail.com>
2022-02-21 13:34:32 +0300

    Adding initial source code

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Eugene on 31.12.21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
